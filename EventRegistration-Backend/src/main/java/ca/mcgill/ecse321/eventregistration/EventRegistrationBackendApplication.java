655fd469bd06d4e1b541a27099ea3d527e48121c
Enzo2806 <enzo.benoit-jeannin@mail.mcgill.ca>
2022-01-19 00:40:37 +0100

    Initial commit to the backend application

---
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/EventRegistrationBackendApplication.java
@@ -0,0 +1,21 @@
+package ca.mcgill.ecse321.eventregistration;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication; // allows gradle to know we re using springboot app
+import org.springframework.boot.SpringApplication; // need to run sopring app using framework spring
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping; //all part of spring framework
+
+@RestController //allows us to access rets endpoint in our application
+@SpringBootApplication
+public class EventRegistrationBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EventRegistrationBackendApplication.class, args);
+  }
+
+  @RequestMapping("/") //http request mapping for hello world, lead us to hello world page
+  public String greeting(){
+    return "Hello world!";
+  }
+
+}
---
+++ EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/eventregistration/EventRegistrationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.eventregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+//not much to do, JUnit will run our test. Make sure package name is consistent.
+@SpringBootTest
+class EventRegistrationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
